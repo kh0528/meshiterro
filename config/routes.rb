60a121305d3656a0e82bec85ea8bc6ae34baf43f
kh0528 <chuyangchuanbin586@gmail.com>
2022-04-26 08:18:33 +0000

    Finish4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   root to: "homes#top"
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
